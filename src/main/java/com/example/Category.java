9c78489e40fc638a94525f4bbf82a587105e7ad5
Edvin Karlsson <edvinkarlsson4@gmail.com>
2025-10-16 23:49:25 +0200

    La till equals, hashcode och to string i category

--- src/main/java/com/example/Category.java
+++ src/main/java/com/example/Category.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public final class Category {
     private final String name;
@@ -26,5 +27,23 @@ public final class Category {
     }
     public String getName(){
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                " Name ='" + name + '\'' +
+                '}';
     }
 }
